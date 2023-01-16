2505380f6e932d015d1a1442750a64955b9dc49c
Faz131 <fazms@icloud.com>
2023-01-15 17:20:02 -0700

    made some changes to JS code

--- Develop/script copy.js
+++ Develop/script copy.js
@@ -1,43 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var preferedLength = document.getElementById('length');
+var passwordText = document.querySelector("#password");
+var numberLength = document.getElementById('lengthNumber')
+
 
 
 
 // Write password to the #password input
 
 function writePassword() {
-  var length = Number(prompt("Requsted Password Length"))
-  var char = prompt("Requested Charsacters to use")
-  var passwordText = document.querySelector("#password");
+  var complexity = document.getElementById('length').value;
+  var passUpperLower = 'ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz123456789!@#$%^&*()';
+  // var passwordLength = '16';
+  var generatePassword = '';
+  var preferedLength = document.querySelector('length')
 
 
-  // var passwordLength;
-  function password() {
-    var generatePassword = {
-      uppercase: 'ABCDEFGHIJKLMOPQRSTUVWXYZ123456789',
-      lowercase: 'abcdefghijklmopqrstuvwxyz',
-      numbers: '0123456789',
-    };
-    var characterChoice = generatePassword[char.toLocaleLowerCase()];
 
-    var result = '';
-    // var password = generatePassword.writePassword();
+  for (let i = 0; i <= complexity; i++) {
+    var complexity = Math.floor(Math.random() * passUpperLower.length);
+    generatePassword += passUpperLower.substring(complexity, complexity + 1);
 
-    // var passwordText = document.querySelector("#password");
-
-
-    // const passUpperLower = 'ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz123456789';
-    // const passwordLength = 8;
-
-
-    for (i = 0; i < length; i++) {
-      result += generatePassword = Math.floor(Math.random() * generatePassword);
-      // (randomNumber, randomNumber + 1);
-    }
-    return result;
   }
-  passwordText.value = password;
+
+
+
+  document.getElementById('password').value = generatePassword;
+
+
+
 }
+
+
+
+// passwordText.value = generatePassword;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
